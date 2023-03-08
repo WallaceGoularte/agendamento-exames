0620bdb553fcdc610391a339ccce61d22bf72028
Wallace Oliveira <wallace.goularte@hotmail.com>
2023-03-08 19:03:13 -0300

    Criação do Projeto

---
+++ src/main/java/com/tcc/agendamentoexame/AgendamentoexameApplication.java
@@ -0,0 +1,13 @@
+package com.tcc.agendamentoexame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoexameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentoexameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcc/agendamentoexame/AgendamentoexameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcc.agendamentoexame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoexameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
